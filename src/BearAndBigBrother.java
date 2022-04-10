cdbb72ba6a3b2e814b15896e15c7c03c59fefdf9
shady00s <ssk2310@hotmail.com>
2022-04-10 22:17:35 +0200

    sunday 10/ April / 2022

--- src/BearAndBigBrother.java
+++ src/BearAndBigBrother.java
@@ -1,5 +1,33 @@
-// Ploblem link  http://codeforces.com/contest/791/problem/A
+import java.util.Scanner;
+
+// Problem link  http://codeforces.com/contest/791/problem/A
 
 public class BearAndBigBrother {
-    
-}
+
+
+     public static void main(String[] args) {
+        
+         Scanner  scan = new Scanner(System.in) ;
+      
+         int limak = scan.nextInt();
+         int bob = scan.nextInt();
+         //limak weight is TRIPLED after each year , bob weight DOUBLED
+        int year = 0;
+      
+      
+
+      
+          while( limak <= bob){
+             limak *= 3 ;
+      
+             bob *= 2 ;
+
+            year ++;
+          }
+          System.out.println(year);
+          scan.close();
+          }
+    }
+
+
+
